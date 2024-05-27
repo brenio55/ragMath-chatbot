93d9ca2454262b018cc547ae62d77bc798e856b6
Brenio Filho <brenio55@outlook.com>
2024-05-26 22:37:10 -0300

    test: testing cors again

--- api/requireResponseOpenAI.js
+++ api/requireResponseOpenAI.js
@@ -29,17 +29,7 @@ if (!apiKey) {
 
 const openAI = new OpenAI({ apiKey });
 
-const corsOptions = {
-   origin: '*', 
-   credentials: true,
-   optionSuccessStatus: 200,
-};
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.options('*', cors())
 
 // Store thread information
 const threads = {};
